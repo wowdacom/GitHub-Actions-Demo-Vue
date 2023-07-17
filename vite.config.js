ab5f8f77077c56c38594ab26f34b42b090c7f618
g7834 <wowdacom.hu@housefun.com.tw>
2023-07-17 10:09:36 +0100

    路徑感覺怪怪的

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:
-    process.env.NODE_ENV !== "production" ? "./" : "/GitHub-Actions-Demo-Vue/",
   plugins: [vue()],
 });
