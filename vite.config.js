7dc11c22d3d92e10f2a514b2a299e8c4ef04f79b
g7834 <wowdacom.hu@housefun.com.tw>
2023-07-17 10:17:09 +0100

    新增路徑

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+const version = +new Date();
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:
+    process.env.NODE_ENV !== "production" ? "./" : "/GitHub-Actions-Demo-Vue/",
   plugins: [vue()],
+  build: {
+    manifest: false,
+    cssCodeSplit: false,
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "index.html"),
+      },
+      output: {
+        entryFileNames: `assets/[name]_${version}.js`,
+        chunkFileNames: `assets/[name]_${version}.js`,
+        assetFileNames: `assets/index_${version}.[ext]`,
+      },
+    },
+  },
 });
