78ac724a8c86fa4ebf4f81c615b7420bbc6e76ec
g7834 <wowdacom.hu@housefun.com.tw>
2023-07-17 10:28:52 +0100

    加上路徑設定呢

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,31 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
+import process from "process";
+
+const version = +new Date();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:
-    process.env.NODE_ENV !== "production" ? "./" : "/GitHub-Actions-Demo-Vue/",
+  base: process.env.NODE_ENV !== "production" ? "./" : "/curious-mind-site/",
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    manifest: false,
+    cssCodeSplit: false,
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "index.html"),
+      },
+      output: {
+        entryFileNames: `assets/[name]_${version}.js`,
+        chunkFileNames: `assets/[name]_${version}.js`,
+        assetFileNames: `assets/index_${version}.[ext]`,
+      },
+    },
+  },
 });
