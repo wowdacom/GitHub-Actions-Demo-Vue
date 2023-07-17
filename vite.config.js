87b2ecd62c66e6ea51b0387a2b748707f3aa92ea
g7834 <wowdacom.hu@housefun.com.tw>
2023-07-17 10:04:25 +0100

    調整路徑

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:
+    process.env.NODE_ENV !== "production" ? "./" : "/GitHub-Actions-Demo-Vue/",
   plugins: [vue()],
-})
+});
