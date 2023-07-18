3925af49d39f344d216423007874c6947e294702
g7834 <wowdacom.hu@housefun.com.tw>
2023-07-18 03:36:13 +0100

    測試是否可以直接 push

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from "./components/HelloWorld.vue";
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Github Hook" />
+  <HelloWorld msg="GitHub-Actions-Demo-Vue" />
 </template>
 
 <style scoped>
